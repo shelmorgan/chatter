82ebfb0534802d2af8770a593762c6a676933013
shelbymorganb <shelbymorganbell@gmail.com>
2015-11-08 00:48:55 -0500

    initial commit. files generated from https://github.com/KyperTech/generator-webpack-redux-react

---
+++ app/components/Message/Message.js
@@ -0,0 +1,17 @@
+import React, {Component, PropTypes} from 'react';
+import './Message.scss';
+
+class Message extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="Message">
+
+      </div>
+    );
+  }
+}
+
+export default Message
