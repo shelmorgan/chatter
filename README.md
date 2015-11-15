59bb9a93fdb0bed9676300ff5aba6b076c461fd8
shelbymorganb <shelbymorganbell@gmail.com>
2015-11-15 03:47:51 -0500

    updated homepage and readme info

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Chatter
-## 
+# **Chatter**
+## Simple messaging app built with React.js and [Matter](https://github.com/KyperTech/matter) using [Webpack-Redux-React](https://github.com/KyperTech/webpack-redux-react-starter)
+
+
+### **TODO:**
+
+* finish auth through tesselate/matter/authrocket
+* app styling
+* add delete button to messages
+* create different pages for all messages and individual conversations
--- app/containers/Home/Home.js
+++ app/containers/Home/Home.js
@@ -7,9 +7,7 @@ export default class Home extends Component {
     return (
       <div className="homepage">
         <h2>Chatter</h2>
-        <p>Simple messaging app built with react and
-          <a href="https://github.com/KyperTech/matter"> Matter</a>
-        </p>
+        <p>Simple messaging app built with React.js and <a href="https://github.com/KyperTech/matter"> Matter </a> using <a href="https://github.com/KyperTech/webpack-redux-react-starter"> Webpack-Redux-React</a></p>
         <p>See more here:
           <a href="https://github.com/shelmorgan/chatter"> GitHub</a>
         </p>
