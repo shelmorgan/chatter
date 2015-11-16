7be756b693dd957975cd7b7f1832ccada0c89f68
shelbymorganb <shelbymorganbell@gmail.com>
2015-11-15 23:51:03 -0500

    updated about page

--- app/containers/About/About.js
+++ app/containers/About/About.js
@@ -3,5 +3,10 @@ import React, {Component} from 'react';
 export default class About extends Component {
 
   render() {
+    return (
+      <div>
+        <h1>About Chatter</h1>
+      </div> //Without div this will not return bc empty return
+    )
   }
 }
