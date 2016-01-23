027886cbe17862df2954f011f5b05103b29f5df2
Shel Morgan <shelbymorganbell@gmail.com>
2016-01-23 00:38:01 -0500

    included material-ui

--- app/root.js
+++ app/root.js
@@ -1,6 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import AppRouter from './app-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+  //Needed for onTouchTap
+  //Can go away when react 1.0 release
+  //Check this repo:
+  //https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 
 class Root extends Component {
